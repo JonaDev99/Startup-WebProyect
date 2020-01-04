ea0f324764ff90131b657d18b74d84336c6bb3ea
Jonatan Contreras Nuño <Jonatan.Contreras@agil.net>
2020-01-03 19:48:55 -0600

    Responsive navbar

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,14 @@
-import { showAlert } from './messages';
-
 //importamos el archivo de css
 import '../scss/style.scss';
 
+const header = document.querySelector('.main-header');
 
-document.getElementById('btn-alert')
-    .addEventListener('click', showAlert);
+window.addEventListener('scroll', () => {
+    const scrollPos = window.scrollY;
+    if(scrollPos > 0){
+        header.classList.add('scrolled');
+        console.log("scroll");
+    } else{
+        header.classList.remove('scrolled');
+    }
+})
--- src/js/messages.js
+++
@@ -1,4 +0,0 @@
-import {v4} from 'uuid' 
-export function showAlert(){
-    alert("Este es el uuid: "+ v4());
-}
