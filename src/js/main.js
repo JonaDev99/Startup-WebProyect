fc05907001a3a7e6ba8a028906cd8fe907adb46d
Jonatan Contreras <develop@MacBook-Air-de-Jonatan.local>
2020-01-04 14:09:23 -0600

    Animations added

--- src/js/main.js
+++ src/js/main.js
@@ -2,7 +2,7 @@
 import '../scss/style.scss';
 
 const header = document.querySelector('.main-header');
-
+const menuBtn = document.querySelector('.menu-btn');
 window.addEventListener('scroll', () => {
     const scrollPos = window.scrollY;
     if(scrollPos > 0){
@@ -11,4 +11,25 @@ window.addEventListener('scroll', () => {
     } else{
         header.classList.remove('scrolled');
     }
-})
+});
+
+/**const clas = window.document.getElementsByClassName("nav-links");
+window.addEventListener('click', (e) => {
+  if(e.clas[0]==clas[0]){
+      console.log("click en la clase");
+  }
+});
+ */
+
+
+
+/*
+const menu = window.document.getElementById("menu-btn");
+window.addEventListener("click", (e)=> {
+    console.log("clicks");
+    if(e.menu==menu){
+        console.log("clicks"+ menu);
+        menu.checked = false;
+    }    
+},false);
+*/
