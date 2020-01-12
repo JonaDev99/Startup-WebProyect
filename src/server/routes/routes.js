c7f2ddaadd176cd747a5ac28a6533df555a1ca01
Jonatan Contreras Nuño <Jonatan.Contreras@agil.net>
2020-01-11 19:10:59 -0600

    Enable the email part

--- src/server/routes/routes.js
+++ src/server/routes/routes.js
@@ -3,11 +3,12 @@ const nodemailer = require('nodemailer');
 const router = Router();
 
 
-router.post('/send-email',(req,res) =>{
+router.post('/send-email', async (req,res) =>{
     const {name, email, phone, enterprice, description} = req.body;
     //Este es el formato de nuestro correo
     contentHMTL = `
     <h1>User information: </h1>
+
     <ul>
         <li>Username: ${name}</li>
         <li>User Email: ${email}</li>
@@ -17,31 +18,32 @@ router.post('/send-email',(req,res) =>{
     </ul>
     <p>Message</p>
     `;
-    /*
     const transporter = nodemailer.createTransport({
         //Aqui van los datos del host de nuestro correo el cual contratamos
-        host: 'mail.digitalsupport.com',
-        port: 26,
+        host: 'mail.caeva-demos.com',
+        //El puerto puede ser 25 o 587 si se trata de un servidor personal 
+        port: 25,
         //Aqui se determina si seran con SSL(true) o sin SSL(false)
         secure: false,
         //en auth utilizaremos 
         auth:{
-            user: 'contact@digitalsupport.com',
-            pass: 'password'
+            user: 'test@caeva-demos.com',
+            pass: 'Adm1nistrador'
         },
         tls: {
             rejectUnauthorized: false
         }
     });
    const info =  await transporter.sendMail({
-        from: "'DigitalSupport <contact@digitalsupport.com>'",
+        from: "'DigitalSupport' <test@caeva-demos.com>",
         to: email,
         subject: 'Nos complace ponernos en contacto con usted',
         html: contentHMTL
     });
     console.log("Mesagge sent", info.messageId);
-*/
-    res.redirect("/");
+    
+    //res.send(contentHMTL);
+    res.redirect('/);
 });
 
 module.exports = router;
